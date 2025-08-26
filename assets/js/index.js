56522b17657ec8fe0ce28ef4ae179dc893d68bbb
SarasiPrarthana <prarthanasarasi32@gmail.com>
2025-08-26 18:16:47 +0530

    start movies.html page structure and layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,3 +23,29 @@ window.addEventListener("click", (e) => {
         movieIframe.src = movieIframe.src;
     }
 });
+
+const watchBtn1 = document.getElementById("watchBtn1");
+const videoModal1 = document.getElementById("videoModal1");
+const closeBtn1 = document.getElementById("closeBtn1");
+const movieVideo1 = document.getElementById("movieVideo iframe1");
+
+watchBtn1.addEventListener("click", () =>{
+    videoModal1.classList.remove("hidden");
+    // movieVideo.play();
+});
+
+closeBtn1.addEventListener("click", () =>{
+    // movieVideo.src = movieVideo.src;
+    // movieVideo.pause();
+    // movieVideo.currentTime = 0;
+    videoModal1.classList.add("hidden");
+    movieIframe1.src = movieIframe1.src;
+});
+
+window.addEventListener("click", (e) => {
+    if(e.target === videoModal1){
+        // movieVideo.src = movieVideo.src;
+        videoModal1.classList.add("hidden");
+        movieIframe1.src = movieIframe1.src;
+    }
+});
